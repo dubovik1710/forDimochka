086ad433f52d72dd8f403d2ea9192e2cf48de4bc
dubovik1710 <146095587+dubovik1710@users.noreply.github.com>
2023-10-09 22:10:46 +0300

    Add files via upload

---
+++ BubbleSort1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Введите количество элементов в массиве: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("Введите элемент {0}: ", i + 1);
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        BubbleSort.Sort(arr);
+
+        Console.WriteLine("Отсортированный массив:");
+        foreach (int element in arr)
+            Console.Write(element + " ");
+    }
+}
+
+public class BubbleSort
+{
+    public static void Sort(int[] array)
+    {
+        int n = array.Length;
+        bool swapped;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            swapped = false;
+
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (array[j] > array[j + 1])
+                {
+                    // Обмен значений
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+
+                    swapped = true;
+                }
+            }
+
+            if (!swapped)
+                break;
+        }
+    }
+}
---
+++ CocktailSort/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Введите количество элементов в массиве: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("Введите элемент {0}: ", i + 1);
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        CocktailSort.Sort(arr);
+
+        Console.WriteLine("Отсортированный массив:");
+        foreach (int element in arr)
+            Console.Write(element + " ");
+    }
+}
+
+public class CocktailSort
+{
+    public static void Sort(int[] array)
+    {
+        int left = 0;
+        int right = array.Length - 1;
+
+        while (left <= right)
+        {
+            for (int i = left; i < right; i++)
+            {
+                if (array[i] > array[i + 1])
+                {
+                    Swap(ref array[i], ref array[i + 1]);
+                }
+            }
+            right--;
+
+            for (int i = right; i > left; i--)
+            {
+                if (array[i - 1] > array[i])
+                {
+                    Swap(ref array[i - 1], ref array[i]);
+                }
+            }
+            left++;
+        }
+    }
+
+    private static void Swap(ref int a, ref int b)
+    {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+}
